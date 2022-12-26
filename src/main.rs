83da2b03d11987d81bfe9ce02ecaae536baccab1
Christoph Coijanovic <christoph.coijanovic@kit.edu>
2022-12-26 16:53:07 +0100

    It moves!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::{thread, time};
+
+const SLEEP_INTERVAL : time::Duration = time::Duration::from_millis(100);
+
+enum Direction {
+ Up,
+ Down,
+ Left,
+ Right,
+}
+
 struct Snake {
     kind: char,
     body: Vec<(u8, u8)>, 
@@ -7,8 +18,19 @@ impl Snake {
     fn new(kind: char) -> Snake {
         Snake {
             kind,
-            body: vec![(0,0), (1,0), (2,0), (3,0), (4,0)],
+            body: vec![(4,0), (3,0), (2,0), (1,0), (0,0)],
         }
+    }
+
+    fn step(&mut self, field: &Field, dir: Direction) {
+        let _ = self.body.pop();
+        let new_head : (u8, u8) = match dir {
+            Direction::Up => (self.body[0].0, (self.body[0].1 + field.height - 1) % field.height),
+            Direction::Down => (self.body[0].0, (self.body[0].1 + 1) % field.height), 
+            Direction::Left => ((self.body[0].0 + field.width - 1) % field.width, self.body[0].1),
+            Direction::Right => ((self.body[0].0 + 1) % field.width, self.body[0].1),
+        };
+        self.body.insert(0, new_head);
     }
 }
 
@@ -16,32 +38,41 @@ struct Field {
     width: u8,
     height: u8,
     kind: char,
-    snake: Snake,
 }
 
 impl Field {
-    fn print(&self) {
+    fn print(&self, snake: &Snake) {
         for i in 0..self.height {
             for j in 0..self.width {
                 // check if there's a snake
-                if self.snake.body.contains(&(j,i)) {
-                    print!("{}", self.snake.kind);
+                if snake.body.contains(&(j,i)) {
+                    print!("{}", snake.kind);
                 } else {
                     print!("{}", self.kind);
                 }
             }
             print!("\n");
         }
+        print!("\n");
     }   
 }
 
 fn main() {
-    let s = Snake::new('🟠');
+    let mut s = Snake::new('🟠');
     let f = Field {
        width: 30,
        height: 20,
        kind: '⚫',
-       snake: s,
     };
-    f.print();
+    loop {
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+        s.step(&f, Direction::Right);
+        f.print(&s);
+        thread::sleep(SLEEP_INTERVAL);
+
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+        s.step(&f, Direction::Up);
+        f.print(&s);
+        thread::sleep(SLEEP_INTERVAL);
+    }
 }
